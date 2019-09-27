4ef1aa3b45ab4c4696d6454fee21d690d15299cd
DPelagio <pelagio3@gmail.com>
2019-09-27 07:45:44 -0500

    Added exercise 5 and changes to exercise 4

--- Sem7/lenguajes/ejercicio4.rkt
+++ Sem7/lenguajes/ejercicio4.rkt
@@ -5,6 +5,8 @@ Daniel Pelagio
 program to find the minimum number divisible up to certain number
 |#
 
+#lang racket
+
 (define (divisible? test limit)
 	(if (= limit 1) 
 		#t
@@ -28,3 +30,21 @@ program to find the minimum number divisible up to certain number
 		)
 	)
 )
+
+;Writing to a file
+;Overwriting
+
+(define out (open-output-file "test.txt" #:exists 'truncate))
+(write "This is only a test 1\n" out)
+(display "This is only a test 2\n" out)
+(display "This is only a test 3\n" out)
+(display "This is only a test 4\n" out)
+(close-output-port out)
+
+;read from a file
+
+(define in (open-input-file "test.txt"))
+(read-line in)
+(close-input-port in)
+
+
